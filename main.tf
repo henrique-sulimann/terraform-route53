4c8356b4a852c6d98c447222941b30f1c99c4a0a
Henrique Sulimann <henrique.sulimann@gmail.com>
2021-01-31 15:20:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-terraform {
+/* terraform {
   backend "s3" {
     bucket = "your bucket name with your tfstate"
     key = "terraform.tfstate"
     region = "us-east-1"
   }
-}
+} */
 
 provider "aws" {}
